a2b60544800b58975874719ac63bc2c1a4a965d7
Vitaly Ostapenko <Ostapenko.Vitaly@gmail.com>
2012-08-27 11:15:21 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+VitalySampleApp2ndEd::Application.config.secret_token = 'b8d2e69f9a4758dc501197774b12675325aae2dd482e02b9091044c0351dba9603414abc63def6b5351407b69fc9554c78ab6a8b4b345ebaca96ff625e5172e7'
