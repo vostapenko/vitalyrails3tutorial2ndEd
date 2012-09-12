4ac6e9890766dba96e5d6464ef8f0fea4b83dda8
Vitaly Ostapenko <Ostapenko.Vitaly@gmail.com>
2012-09-12 14:49:37 +0000

    Make a basic User model (including secure passwords)

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 VitalySampleApp2ndEd::Application.routes.draw do
 
-  get "users/new"
-
   root to: 'static_pages#home'
 
   match '/signup',  to: 'users#new'
---
+++ db/structure.sql
@@ -0,0 +1,21 @@
+CREATE TABLE `schema_migrations` (
+  `version` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  UNIQUE KEY `unique_schema_migrations` (`version`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `email` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `created_at` datetime NOT NULL,
+  `updated_at` datetime NOT NULL,
+  `password_digest` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `index_users_on_email` (`email`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+INSERT INTO schema_migrations (version) VALUES ('20120912083053');
+
+INSERT INTO schema_migrations (version) VALUES ('20120912132006');
+
+INSERT INTO schema_migrations (version) VALUES ('20120912140216');
