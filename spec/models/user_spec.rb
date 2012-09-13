cbf7f2d478be68aa7deae788404c742051cea42b
Vitaly Ostapenko <Ostapenko.Vitaly@gmail.com>
2012-09-13 07:51:46 +0000

    Complete exercise for chapter 6

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -54,10 +54,20 @@ describe User do
     end
   end
 
+  describe "email address with mixed case" do
+    let(:mixed_case_email) { "Foo@ExAMPle.CoM" }
+
+    it "should be saved as all lower-case" do
+      @user.email = mixed_case_email
+      @user.save
+      @user.reload.email.should == mixed_case_email.downcase
+    end
+  end
+
   describe "when email address is already taken" do
     before do
       user_with_same_email = @user.dup
-      user_with_same_email.email = @user.email.upcase
+      user_with_same_email.email = @user.email.downcase
       user_with_same_email.save
     end
 
